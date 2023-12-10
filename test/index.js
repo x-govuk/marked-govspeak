1f3595a6c4f6e8c06f24f96eaf00d6fee8157f6e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-10 19:02:08 +0000

    Use node test runner

---
+++ test/index.js
@@ -0,0 +1,97 @@
+const assert = require('assert/strict')
+const { describe, it } = require('node:test')
+const { marked } = require('marked')
+const govspeak = require('../index.js')
+
+describe('Govspeak', () => {
+  marked.use({ extensions: govspeak })
+
+  it('Renders address', () => {
+    assert.equal(
+      marked('$A\nLine 1  \nLine 2\n$A'),
+      '<address class="govspeak-address h-adr">\n  <p>Line 1<br>Line 2</p>\n\n</address>'
+    )
+  })
+
+  it('Renders button', () => {
+    assert.equal(
+      marked('{button}[Click me](https://gov.uk){/button}'),
+      '<p><a class="govuk-button" href="https://gov.uk" role="button">Click me</a></p>\n'
+    )
+  })
+
+  it('Renders start button', () => {
+    assert.equal(
+      marked('{button start}[Click me](https://gov.uk){/button}'),
+      '<p><a class="govuk-button govuk-button--start" href="https://gov.uk" role="button">Click me<svg class="govuk-button__start-icon" xmlns="http://www.w3.org/2000/svg" width="17.5" height="19" viewBox="0 0 33 40" aria-hidden="true" focusable="false"><path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z"/></svg></a></p>\n'
+    )
+  })
+
+  it('Renders call to action', () => {
+    assert.equal(
+      marked('$CTA\nCall to action\n$CTA'),
+      '<div class="govspeak-call-to-action">\n  <p>Call to action</p>\n\n</div>'
+    )
+  })
+
+  it('Renders contact', () => {
+    assert.equal(
+      marked('$C\nPhone  \nEmail\n$C'),
+      '<div class="govspeak-contact">\n  <p>Phone<br>Email</p>\n\n</div>'
+    )
+  })
+
+  it('Renders form download', () => {
+    assert.equal(
+      marked('$D\n[Download (PDF, 14KB)](https://example.com/file.pdf)\n$D'),
+      '<div class="govspeak-form-download">\n  <p><a href="https://example.com/file.pdf">Download (PDF, 14KB)</a></p>\n\n</div>'
+    )
+  })
+
+  it('Renders example', () => {
+    assert.equal(
+      marked('$E\nExample\n$E'),
+      '<div class="govspeak-example">\n  <p>Example</p>\n\n</div>')
+  })
+
+  it('Renders information callout', () => {
+    assert.equal(
+      marked('^information^'),
+      '<div class="govspeak-information-callout" role="note" aria-label="Information">\n  <p>information</p>\n</div>'
+    )
+  })
+
+  it('Renders information', () => {
+    assert.equal(
+      marked('$I\ninformation\n$I'),
+      '<div class="govspeak-information">\n  <p>information</p>\n\n</div>'
+    )
+  })
+
+  it('Renders place', () => {
+    assert.equal(
+      marked('$P\nThis is a place\n$P'),
+      '<div class="govspeak-place">\n  <p>This is a place</p>\n\n</div>'
+    )
+  })
+
+  it('Renders stat headline', () => {
+    assert.equal(
+      marked('{stat-headline}\n*13.8bn* years since the big bang{/stat-headline}'),
+      '<div class="govspeak-stat-headline">\n  <p><em>13.8bn</em> years since the big bang</p>\n\n</div>'
+    )
+  })
+
+  it('Renders steps', () => {
+    assert.equal(
+      marked('s1. Add numbers.\ns2. Check numbers.\ns3. Love numbers.'),
+      '<ol class="govspeak-steps">\n  <li>Add numbers.</li>\n  <li>Check numbers.</li>\n  <li>Love numbers.</li>\n</ol>'
+    )
+  })
+
+  it('Renders warning callout', () => {
+    assert.equal(
+      marked('%warning%'),
+      '<div class="govspeak-warning-callout" role="note" aria-label="Warning">\n  <p>warning</p>\n</div>')
+  })
+})
